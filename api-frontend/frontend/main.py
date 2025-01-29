04fe6e91377e91a7bcc53073604849dd03c9df80
Massyl GH <massyl-6@hotmail.fr>
2025-01-29 22:29:47 +0100

    Add api frontend

---
+++ api-frontend/frontend/main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+
+from settings import API_URL, TITLE
+from components import build_data_plot
+
+
+st.set_page_config(page_title=TITLE)
+st.title(TITLE)
+
+area_response = requests.get(API_URL / "area_values")
+
+area = st.selectbox(
+    label="Denmark is divided in two price areas, or bidding zones,\
+        divided by the Great Belt. DK1 (shown as 1) is west of the Great Belt \
+            and DK2 (shown as 2) is east of the Great Belt.",
+    options=area_response.json().get("values", [])
+)
+
+consumer_type_response = requests.get(API_URL / "consumer_type_values")
+
+consumer_type = st.selectbox(
+    label="The consumer type is the Industry Code DE35 which is owned \
+          and maintained by Danish Energy, a non-commercial lobby \
+              organization for Danish energy companies. \
+                The code is used by Danish energy companies.",
+    options=consumer_type_response.json().get("values", []),
+)
+
+if area and consumer_type:
+    st.plotly_chart(build_data_plot(area, consumer_type))
